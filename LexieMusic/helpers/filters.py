f1f39b1f6c8cb34fee0342b54a70a9c786bf7047
Vihara Senindu <86075472+viharasenindu@users.noreply.github.com>
2021-08-29 11:54:04 +0530

    Lexie

--- LexieMusic/helpers/filters.py
+++ LexieMusic/helpers/filters.py
@@ -20,7 +20,7 @@ from typing import Union
 
 from pyrogram import filters
 
-from LexieLMusic.config import COMMAND_PREFIXES
+from LexieMusic.config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (
